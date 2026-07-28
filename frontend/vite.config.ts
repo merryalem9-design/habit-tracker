14916a3747902846cc795c67b5fd95d989763135
Mariyamawit Alemseged <merryalem9@gmail.com>
2026-07-28 16:01:30 +0300

    install @tailwindcss/vite plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite' // <--- This line
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(), // <--- And this line
+  ],
 })
